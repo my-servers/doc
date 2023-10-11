8fb248fc6ff0a5a485ec20e1a3326e34a85cd988
kevenqiu <kevenqiu@tencent.com>
2023-10-11 18:58:17 +0800

    up

--- static/google.js
+++ static/google.js
@@ -2,3 +2,5 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-FP67187K3W');
+
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "adcf0ad12cc94526b5fddd1eec23339d"}'></script>
