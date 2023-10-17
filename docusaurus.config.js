89e318a3843f617ac90ae0b73061d0479012182b
kevenqiu <kevenqiu@tencent.com>
2023-10-17 13:27:08 +0800

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/newMyServer1024.png',
 
   // Set the production url of your site here
-  url: 'https://myservers.codeloverme.cn,
+  url: 'https://myservers.codeloverme.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
