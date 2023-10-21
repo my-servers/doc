6b1e1b872edb9844a2a7e12f9fe3ba6849b944bf
kevenqiu <kevenqiu@tencent.com>
2023-10-21 21:00:50 +0800

    download img

--- static/google.js
+++ static/google.js
@@ -5,5 +5,13 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?97121c966908fdb33bc4437555a2d772";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-  gtag('event', 'conversion', {'send_to': 'AW-11371629200/Zs2rCObng-8YEJCVta4q'});
 })();
+
+document.write("<script async src='https://www.googletagmanager.com/gtag/js?id=G-4966Y3DK52'></script>");
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-4966Y3DK52');
+gtag('event', 'conversion', {'send_to': 'AW-11371629200/Zs2rCObng-8YEJCVta4q'});
