32cbb158e54cd1ec5bf5f49b43041c1c6cbf93b4
kevenqiu <kevenqiu@tencent.com>
2023-11-03 00:53:53 +0800

    img

--- config.js
+++ config.js
@@ -4,5 +4,12 @@ export default {
             {name:"文档", url:"./"},
             {name:"Github", url:"./"},
         ]
+    },
+    App:{
+        ScreenShotFirst:"https://myservers.codeloverme.cn/img/service.webp",
+        ScreenShotSecond:"https://myservers.codeloverme.cn/img/app.webp",
+        Descript:"一个App监控管理个人所有的服务器和应用。",
+        Name:"MyServers",
+        Logo:"https://myservers.codeloverme.cn/img/newMyServer1024.png",
     }
 }
---
+++ src/components/Plugin.tsx
@@ -0,0 +1,7 @@
+export function Plugin(plugin){
+    return (
+        <div>
+            <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDFDc_TWYSTGDZcrAR3J4aesn3Z6R3vDei84-67eVcnQ&s"></img>
+        </div>
+    )
+}
