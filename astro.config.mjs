f836276e72605b3eb0e015556d7d709dbd559aba
kevenqiu <kevenqiu@tencent.com>
2023-11-08 22:28:13 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), solidJs()]
+  site: 'https://myservers.codeloverme.cn',
+  integrations: [preact(), tailwind(), solidJs(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // 选择 Shiki 内置的主题（或添加你自己的主题）
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'one-dark-pro',
+      // 添加自定义语言
+      // 注意：Shiki 内置了无数语言，包括 .astro！
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // 启用自动换行，以防止水平滚动
+      wrap: true
+    }
+  }
 });
--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,5 +1,5 @@
 export function NavLink(link){
     return (
-        <a class="mr-5  hover:bg-gray-900 hover:text-white hover:shadow-xl ease-in-out transition p-3 rounded-md" href={link.url}>{link.name}</a>
+        <a class="mr-5 text-gray-300 hover:bg-gray-900 hover:text-white hover:shadow-xl ease-in-out transition p-3 rounded-md" href={link.url}>{link.name}</a>
     )
 }
